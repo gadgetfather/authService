590b47fddaa46fb1306a9f68875d50e1a339a59a
Akshay Kamble <kamblea635@gmail.com>
2023-10-02 12:34:07 +0530

    add: jwt token verify and create

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,7 +3,9 @@ const bcrypt = require("bcrypt");
 dotenv.config();
 const PORT = process.env.PORT;
 const SALT = bcrypt.genSaltSync(10);
+const JWT_SECRET = process.env.JWT_SECRET;
 module.exports = {
   PORT,
   SALT,
+  JWT_SECRET,
 };
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,11 +1,32 @@
 const userRepository = require("../repositories/user-repository");
-
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/serverConfig");
 module.exports = {
   async create(user) {
     try {
       return await userRepository.create(user);
     } catch (error) {
       console.log("Something went wrong in service: ", error);
+      throw error;
+    }
+  },
+
+  // create JWT token
+  createToken(user) {
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+    return jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+  },
+  //verify JWT token
+  verifyToken(token) {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      console.log("Something went wrong in token verify service: ", error);
       throw error;
     }
   },
